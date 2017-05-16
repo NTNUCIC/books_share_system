5e97cceaf9ff360a8dd70b319122c5f58f020ace
Hane Liu <hane0131@gmail.com>
2017-05-16 19:42:49 +0800

    Add temporary homepage

--- book_share_system/urls.py
+++ book_share_system/urls.py
@@ -17,7 +17,9 @@ from django.conf import settings
 from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
+from books.views import home
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', home),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
