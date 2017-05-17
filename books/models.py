a149715d122de4f6b808a164a77e8a9cdd6de0be
Hane Liu <hane0131@gmail.com>
2017-05-17 11:44:07 +0800

    Declare Book's table setting

--- books/models.py
+++ books/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    name = models.CharField(max_length=50)
+    owner = models.CharField(max_length=10)
+    status = models.BooleanField(default=True)
